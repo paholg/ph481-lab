0ac4558cb0aa0f23eaebf36e379a9d83f7f109dc
Michael Perlin <perlinm@MAPHost.MAPDomain>
2014-02-04 11:34:13 -0800

    lab3 plotting stuff

--- lab3/diode-data/plot-diodes.py
+++ lab3/diode-data/plot-diodes.py
@@ -1,9 +1,49 @@
 from __future__ import division
 from pylab import *
+import numpy.fft as fft
+import glob
 
-for i in xrange(1):
-  data = loadtxt('scope_%i_1.csv' %i, delimiter=',', skiprows = 2)
-  plot(data[:,0], data[:,1])
-savetxt('bobos', data)
-show()
-print data
+fig_x = 3
+fig_y = 3
+def make_fig():
+  figure(figsize=(fig_x,fig_y))
+if not 'fontSize' in globals():
+	font_size = 8
+params = {'font.size': font_size,
+          'legend.fontsize': font_size}
+rcParams.update(params)
+
+files = glob.glob('scope*.csv')
+n = zeros(len(files))
+
+make_fig()
+sample = loadtxt(files[0],delimiter=',',skiprows=2)
+plot(sample[:,0]*1000,sample[:,1]*1000,'k')
+xlabel('Time (ms)')
+ylabel('Potential (mV)')
+tight_layout()
+savefig('../diode-sample.pdf')
+
+make_fig()
+for i in xrange(len(files)):
+	data = loadtxt(files[i],delimiter=',',skiprows=2)
+	amplitude = data[:,1] - mean(data[:,1])
+	spectrum = abs(fft.fft(amplitude))
+	dt = data[1,0]-data[0,0]
+	freq = fft.fftfreq(len(spectrum),d=dt)
+	freq = fft.fftshift(freq)
+	spectrum = fft.fftshift(spectrum)
+	plot(freq,spectrum)
+	n[i] = abs(freq[spectrum.tolist().index(spectrum.max())])
+
+s = 10**5
+l = 2*s/n
+print mean(l)
+print std(l)
+
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+xlim(0,1500)
+tight_layout()
+savefig('../FFT.pdf')
+
